1d594a30ae02e01012df63205ab5309329bc9b8d
laladaysz <leandralaura.dias@gmail.com>
2024-11-04 09:40:32 -0300

    :tada: init

---
+++ Parking Lot Management/Model/Usuario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking_Lot_Management.Model
+{
+    public class Usuario
+    {
+        public int Id { get; set; }               
+        public string Nome { get; set; }           
+        public string Email { get; set; }          
+        public string Senha { get; set; }          
+        public string Role { get; set; }           
+        public bool Ativado { get; set; }
+
+        public Usuario(int id, string nome, string email, string senha, string role, bool ativado)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Role = role;
+            Ativado = ativado;
+        }
+
+        public bool ValidarSenha(string senha)
+        {
+            return Senha == senha;
+        }
+    }
+}
---
+++ Parking Lot Management/View/Form1.cs
@@ -0,0 +1,10 @@
+namespace Parking_Lot_Management
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
